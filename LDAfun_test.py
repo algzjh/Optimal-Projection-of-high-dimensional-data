210d8bc79bfc5a0f6c87df5d83213bee7eadfb46
algzjh <jiehuizhou@outlook.com>
2019-01-15 22:14:50 +0800

    Add files via upload
    get discriminating eigenvector using LDA

--- LDAfun_test.py
+++ LDAfun_test.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 def lda(c0, c1):
     """
@@ -8,23 +9,21 @@ def lda(c0, c1):
     """
     # get the basic information
     dim = c0.shape[1]  # the dim is the number of features, for instance, the dim of iris is 4
-    c = np.concatenate((c0, c1))  # vertical stacking, default axis is 0
+    c = np.concatenate((c0, c1))  # Vertical stacking, default axis is 0
     overall_mean = np.mean(c, axis=0)
     print("overall_mean: ", overall_mean)
     num0 = c0.shape[0]  # the number of class-0
     num1 = c1.shape[1]  # the number of class-1
 
     # Computing the mean vectors
-    mean0_vectors = []
-    mean1_vectors = []
     mean0_vectors = np.mean(c0, axis=0)
     mean1_vectors = np.mean(c1, axis=0)
     print("mean0_vectors: ", mean0_vectors)
     print("mean1_vectors: ", mean1_vectors)
 
     # Computing the Scatter Matrices
-    # Within-class scatter matrix S_W
 
+    # Within-class scatter matrix S_W
     S_W = np.zeros((dim, dim))
     class0_sc_mat = np.zeros((dim, dim))
     mv = mean0_vectors.reshape(dim, 1)
@@ -47,7 +46,6 @@ def lda(c0, c1):
     print("within-class Scatter Matrix: \n", S_W)
 
     # Between-class scatter matrix S_B
-
     S_B = np.zeros((dim, dim))
     print("overall_mean: ", overall_mean)
     overall_mean = overall_mean.reshape(dim, 1)
@@ -67,9 +65,9 @@ def lda(c0, c1):
 
     eig_pairs = sorted(eig_pairs, key=lambda k: k[0], reverse=True)
 
-    # print("Eigenvalues in decreasing order:\n")
-    # for i in eig_pairs:
-    #     print(i[0], i[1])
+    print("Eigenvalues in decreasing order:\n")
+    for i in eig_pairs:
+        print(i[0], i[1])
     #
     # print("Variance explained:\n")
     # eigv_sum = sum(eig_vals)
@@ -77,4 +75,46 @@ def lda(c0, c1):
     #     print("eigenvalue {0}: {1:.6%}".format(i+1, (j[0]/eigv_sum).real))
     #
     # print("eig: ", eig_pairs[0][1])
-    return np.real(eig_pairs[0][1])
+    # return np.real(eig_pairs[:][1])
+
+    return [i[1] for i in eig_pairs]
+
+
+def genGaussianData():
+    """
+    this function is to generate Gaussian Data
+    :return: class_sample which contains 2 class
+    each has shape num[i]*2
+    """
+    num = [200, 200]
+    mu_vec = np.array([[5.0, 11.0], [16.0, 2.0]])
+    # print("mu_vec: \n", mu_vec)
+    cov_mat = []
+    for i in range(2):
+        cov_mat.append(np.array([[1, 0], [0, 9]]))
+        # print("i=%d cov_mat: \n"%i, cov_mat)
+    class_sample = []
+    for i in range(2):
+        class_sample.append(np.random.multivariate_normal(mu_vec[i], cov_mat[i], num[i]))
+        # print("i: \n", class_sample[i])
+    return class_sample, num
+
+
+if __name__ == "__main__":
+    class_sample, num = genGaussianData()
+    c0 = class_sample[0]
+    c1 = class_sample[1]
+    plt.plot(c0[:, 0], c0[:, 1], 'o', markersize=8, color='b', alpha=0.3, label='class0')
+    plt.plot(c1[:, 0], c1[:, 1], '^', markersize=8, color='g', alpha=0.3, label='class1')
+    mix = np.min([np.min(c0[:, 0]), np.min(c1[:, 0])])
+    max = np.max([np.max(c0[:, 1]), np.max(c1[:, 1])])
+    print("mix: ", mix, "max: ", max)
+
+    discriminating_vectors = lda(c0, c1)
+    print("discriminating_vectors: \n", discriminating_vectors)
+
+    print("line: \n", discriminating_vectors[0][0], discriminating_vectors[0][1])
+    x = np.linspace(mix, max)
+    k = discriminating_vectors[0][1] / discriminating_vectors[0][0]
+    plt.plot(x, k*x,  color='r')
+    plt.show()
